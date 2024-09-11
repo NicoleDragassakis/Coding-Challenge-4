be90b1549132c7f5cf972fa2c812ebf30e788b71
Nicole Dragassakis <niki_dragasakis@icloud.com>
2024-09-11 16:10:10 -0400

    Initialize inventory with product objects

--- inventory_management.js
+++ inventory_management.js
@@ -1,1 +1,12 @@
 //Inital Setup
+
+//Task 1 invesntory array of product objects
+
+const inventory =[ 
+{ name: "monitor", price: "150.00", quantity: "4", lowStockLevel: "2"},
+{name: "radio", price: "50.00", quantity: "10", lowStockLevel: "1"},
+{name: "television", price: "300.00", quantity: "8", lowStockLevel: "2"},
+{name: "motherboard", price: "200.00", quantity: "5", lowStockLevel: "1"},
+{name: "camera", price: "65.00", quantity: "1", lowStockLevel: "1"},
+];
+console.log (inventory); //consoling to test array
