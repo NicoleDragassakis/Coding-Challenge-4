224eb3a27550981df28e9652ebab566d68c38f58
Nicole Dragassakis <niki_dragasakis@icloud.com>
2024-09-14 21:27:44 -0400

    Create calculateInventoryValue function

--- inventory_management.js
+++ inventory_management.js
@@ -2,6 +2,7 @@
 
 //Task 1 inventory array of product objects
 
+console.log("Task One"); //for better readability on the console.
 const inventory =[ 
 {name: "monitor", price: 150.00, quantity: 4, lowStockLevel: 2},
 {name: "radio", price: 50.00, quantity: 10, lowStockLevel: 1},
@@ -12,7 +13,11 @@ const inventory =[
 console.log (inventory); //consoling to test array
 
 //Task 2 function display product details
+
 //directions Write a function named displayProductDetails that accepts a product object and logs the product’s name, price, quantity in stock, and stock status 
+
+console.log("Task Two"); //for better readability on the console.
+
 function displayProductDetails(product){
     let { name, price, quantity, lowStockLevel} = product;
    
@@ -36,17 +41,21 @@ console.log(displayProductDetails(objectFake));
 
 //requirements write function that accepts product object and number "unitsSold" - "unitsSoldcurrentquantity" function should log product stock status.
 
+console.log("Task Three"); //for better readability on the console.
+
 function updateStock (product, unitsSold,){
     let newQuantity =(product.quantity -= unitsSold);
     const stockLevel = newQuantity <= 0 ? "No Stock" : newQuantity <= product.lowStockLevel ? "Low Stock": 
     console.log("Updated Stock", stockLevel);
 return displayProductDetails(product); }
 
-console.log("Inventory Update" ,updateStock(objectFake, 2)); //Sold Two products and updated Stock Status
+console.log("Inventory Update" ,updateStock(objectFake, 2)); //Sold Two products and updated Stock Status outputs updated stock status
 
 //console.log("Stock Status Update Question" ,updateStock(objectFake, 5)); //test
 
 //task 4 create a function to check low stock products
+
+console.log("Task Four"); //for better readability on the console.
 
 function checkLowStock (inventory){
     const lowStockProducts = inventory.filter(product => product.quantity <= product.lowStockLevel); //filters out the products which do not meet requirement
@@ -54,4 +63,27 @@ function checkLowStock (inventory){
        console.log("Low Stock Items:", `${product.name}: Quantity: ${product.quantity}`); //output "camera: 1"
     });
 }
-checkLowStock(inventory); //calls function
+checkLowStock(inventory); //calls function outputs Low Stock Items: camera: Quantity: 1
+
+//Task 5 Calculate Total Inventory Value
+
+//requirements: function calculateinventoryValue iterates over inventory and returns the total value value is calc by price times quantity use reduce()
+
+console.log("Task Five"); //for better readability on the console.
+
+function calculateInventoryValue(inventory){
+    return inventory.reduce((accumulator, product) => { //accumulator changes starting from 0 each time .reduce itterates over the array
+        return accumulator + (product.price * product.quantity);
+    }, 0);
+}
+console.log("Total Inventory Value" ,calculateInventoryValue(inventory)); //outputs Total Inventory Value 4565
+
+//Task 6 function to process a sale
+
+//requirements function named processSale that accepts product.name and uunitssold to find the product by name, if found call updatestock to reduce quantity if not found log error message.
+
+console.log("Task Six"); //for better readability on the console.
+
+function processSale (){
+
+}
